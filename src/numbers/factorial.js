60ec44662a40294800715c7b68e6611a5534b6c7
Maxime Montagne <maxime@drosalys.fr>
2025-07-11 15:31:47 +0200

    factorial.test pass

--- src/numbers/factorial.js
+++ src/numbers/factorial.js
@@ -12,6 +12,21 @@
  *  @return {number}
  */
 export function factorial(int) {
-    return 0;
+    // Méthode 1 -- For Loop
+    // let value = 1;
+    // for (let i = 2; i <= int; i++) {
+    //     value *= i;
+    // }
+    // return value;
+
+    // Méthode 2 -- Recursivité
+    return int === 0 ? 1 : factorial(int - 1) * int;
+
+    // Méthode 3 -- Déclaratif
+    // return Array(int)
+    //     .fill()
+    //     .map((x, i) => i + 1)
+    //     .reduce((acc,i)=> i >= 2 ? acc * i: i)
+    //     ;
 }
 
