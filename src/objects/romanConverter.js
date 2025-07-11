b74ecf34d1c9e75848ca06844447c485ef381b68
Maxime Montagne <maxime@drosalys.fr>
2025-07-11 16:00:34 +0200

    romanNumber.tests pass

--- src/objects/romanConverter.js
+++ src/objects/romanConverter.js
@@ -13,5 +13,29 @@
  * @returns {string}
  */
 export function romanConverter(integer) {
-    return "";
+    const ROMAN_NUMBERS = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    }
+
+    let converted = "";
+
+    Object.entries(ROMAN_NUMBERS).forEach(([key, value], i) => {
+        const nbOfTime = Math.floor(integer / value);
+        converted += key.repeat(nbOfTime);
+        integer -= nbOfTime * value;
+    });
+
+    return converted;
 }
